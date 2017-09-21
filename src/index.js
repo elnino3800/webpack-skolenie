a3bb82987370b7f57c93412581402559e11d3d32
elnino3800 <lubospit@gmail.com>
2017-09-21 23:17:43 +0200

    code splitting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import "./styles.css";
 
 import sum from "./sum";
-import "./image_viewer";
 
 const header = document.createElement("h1");
 header.innerText = sum(2,3);
 document.body.appendChild(header);
+
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.addEventListener('click', () => {
+    System.import('./image_viewer')
+        .then(module => {
+            console.log(module);
+        });
+});
+
+document.body.appendChild(button);
